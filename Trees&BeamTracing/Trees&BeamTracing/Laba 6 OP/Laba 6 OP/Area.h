57af58f9cd20aba1d1f51d53221118b4c344a996
Vostok17 <Gagarin.Artem.2008@gmail.com>
2021-05-24 23:02:41 +0300

    Area func prototypes

--- Trees&BeamTracing/Trees&BeamTracing/Laba 6 OP/Laba 6 OP/Area.h
+++ Trees&BeamTracing/Trees&BeamTracing/Laba 6 OP/Laba 6 OP/Area.h
@@ -1,5 +1,19 @@
 #pragma once
+
+#include <iostream>
+#include <vector>
+#include "Triangle.h"
+using namespace std;
+
 class Area
 {
+	vector<int> pointMax;
+	vector<int> pointMin;
+public:
+	int count = 0;
+
+	void addToArea(Triangle* newData);
+	bool isInArea(Ray ray);
+	float distToArea(Triangle* from);
 };
 
