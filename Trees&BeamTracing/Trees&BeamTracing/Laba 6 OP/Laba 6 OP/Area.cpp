e83107d6f82f261806b5f84f0ad744479d55aeff
Vostok17 <Gagarin.Artem.2008@gmail.com>
2021-05-25 02:24:42 +0300

    Add to tree

--- Trees&BeamTracing/Trees&BeamTracing/Laba 6 OP/Laba 6 OP/Area.cpp
+++ Trees&BeamTracing/Trees&BeamTracing/Laba 6 OP/Laba 6 OP/Area.cpp
@@ -1,12 +1,16 @@
 #include "Area.h"
 
-void Area::addToArea(Triangle* newData) {
-    for (int i = 0; i < 3; i++) {
-        if (count == 0) {
+void Area::addToArea(Triangle* newData) 
+{
+    for (int i = 0; i < 3; i++) 
+    {
+        if (count == 0) 
+        {
             pointMax.push_back(newData->getMax(i));
             pointMin.push_back(newData->getMin(i));
         }
-        else {
+        else 
+        {
             if (newData->getMax(i) > pointMax[i]) {
                 pointMax[i] = newData->getMax(i);
             }
@@ -18,17 +22,22 @@ void Area::addToArea(Triangle* newData) {
     count++;
 }
 
-bool Area::isInArea(Ray ray) {
+bool Area::isInArea(Ray ray) 
+{
     vector<float> minT;
     vector<float> maxT;
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 3; i++) 
+    {
         minT.push_back((pointMin[i] - ray.stPoint[i]) / ray.directionVector[i]);
         maxT.push_back((pointMax[i] - ray.stPoint[i]) / ray.directionVector[i]);
     }
     bool flag = true;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            if (minT[i] > maxT[j]) {
+    for (int i = 0; i < 3; i++) 
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (minT[i] > maxT[j]) 
+            {
                 flag = false;
             }
         }
@@ -36,9 +45,11 @@ bool Area::isInArea(Ray ray) {
     return flag;
 }
 
-float Area::distToArea(Triangle* from) {
+float Area::distToArea(Triangle* from) 
+{
     float res = 0;
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 3; i++) 
+    {
         res += (from->getMax(i) - pointMax[i]) * (from->getMax(i) - pointMax[i]);
     }
     return sqrt(res);
---
+++ Trees&BeamTracing/Trees&BeamTracing/Laba 6 OP/Laba 6 OP/Tree.h
@@ -0,0 +1,12 @@
+#include "Node.h"
+
+class Tree 
+{
+
+    Node root;
+    float findInNode(Ray ray, Node* node);
+    void addToNode(Triangle* newTriangle, Node* node);
+public:
+    void addToTree(Triangle* newTriangle);
+    float findInTree(Ray ray);
+};
