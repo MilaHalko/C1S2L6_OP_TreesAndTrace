64d260211ca71ade4c2f1da0c04f95d6cefdb62c
Vostok17 <Gagarin.Artem.2008@gmail.com>
2021-06-02 10:43:42 +0300

    additional task

--- Trees&BeamTracing/Trees&BeamTracing/Laba 6 OP/Laba 6 OP/main.cpp
+++ Trees&BeamTracing/Trees&BeamTracing/Laba 6 OP/Laba 6 OP/main.cpp
@@ -8,15 +8,25 @@ int main()
 {
     Tree tree;
     string path = "cow.obj";
+
     ObjReader reader(path);
     vector<Triangle> data = reader.triangles;
-    for (int i = 0; i < data.size(); i++) {
+
+
+    for (int i = 0; i < data.size(); i++) 
+    {
         tree.addToTree(&data[i]);
     }
+
+
     int h = 720, w = 720;
     Frame frame(h, w, tree);
+
     float** pixelMat = frame.getFrame();
+
+
     bmpWriter writer("test.bmp", pixelMat, h, w);
+
     writer.bmpWrite();
 
     system("pause");
